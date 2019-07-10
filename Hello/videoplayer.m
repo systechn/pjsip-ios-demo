f1c6594e15411eb32bed65fabc9d7e788f46cba9
yanggz <ygz012@163.com>
2019-07-10 21:25:42 +0800

    better

--- Hello/videoplayer.m
+++ Hello/videoplayer.m
@@ -19,146 +19,148 @@ typedef struct Videoplayer_t {
     dispatch_queue_t queue;
     int restart;
     int stop;
-    int isopen;
+    int is_open;
     char uri[256];
-    AVPicture  pAVPicture;
-    AVFormatContext *pAVFormatContext;
-    AVCodecContext *pAVCodecContext;
-    AVFrame *pAVFrame;
-    struct SwsContext *pSwsContext;
-    AVPacket pAVPacket;
-    int videoWidth;
-    int videoHeight;
-    int videoStreamIndex;
-    NSTimeInterval time0;
-    NSTimeInterval time1;
     int restart_count;
-    int index;
+    int frame_index;
     int frame_rate;
+    NSTimeInterval time_restart;
+    NSTimeInterval time_frame;
+    
+    AVPicture picture;
+    AVFormatContext *format_ctx;
+    AVCodecContext *codec_ctx;
+    AVFrame *frame;
+    struct SwsContext *sws_ctx;
+    AVPacket packet;
+    int width;
+    int height;
+    int stream_index;
 } Videoplayer_t;
 
 Videoplayer_t videoplayer = {
-    .restart = 0,
-    .stop = 0,
-    .isopen = 0,
+    .restart = FALSE,
+    .stop = FALSE,
+    .is_open = FALSE,
     .uri = "",
-    .index = 0,
+    .frame_index = 0,
 };
 
+Videoplayer_t *self = &videoplayer;
+
 static int videoplayer_open() {
-    videoplayer.pAVFormatContext = avformat_alloc_context();
-    videoplayer.pAVFrame = av_frame_alloc();
+    self->format_ctx = avformat_alloc_context();
+    self->frame = av_frame_alloc();
     AVDictionary *opts = NULL;
     av_dict_set(&opts, "stimeout", "6000000", 0);
 //    av_dict_set(&opts, "bufsize", "1024000", 0);
     
-    int result = avformat_open_input(&videoplayer.pAVFormatContext, videoplayer.uri, NULL, &opts);
+    int rc = avformat_open_input(&self->format_ctx, self->uri, NULL, &opts);
     
     NSLog(@"after avformat_open_input");
-    if (result < 0){
-        avformat_free_context(videoplayer.pAVFormatContext);
-        av_frame_free(&videoplayer.pAVFrame);
+    if (rc < 0){
+        avformat_free_context(self->format_ctx);
+        av_frame_free(&self->frame);
         NSLog(@"avformat_open_input fail");
         return -1;
     }
     
-    videoplayer.pAVFormatContext->max_analyze_duration = 0.5 * AV_TIME_BASE;
+    self->format_ctx->max_analyze_duration = 0.5 * AV_TIME_BASE;
     
-    result = avformat_find_stream_info(videoplayer.pAVFormatContext, NULL);
-    if (result < 0) {
+    rc = avformat_find_stream_info(self->format_ctx, NULL);
+    if (rc < 0) {
         NSLog(@"avformat_find_stream_info < 0");
-        avformat_free_context(videoplayer.pAVFormatContext);
-        av_frame_free(&videoplayer.pAVFrame);
+        avformat_free_context(self->format_ctx);
+        av_frame_free(&self->frame);
         return -1;
     }
     
     NSLog(@"after avformat_find_stream_info");
-    videoplayer.videoStreamIndex = -1;
-    for (uint i = 0; i < videoplayer.pAVFormatContext->nb_streams; i++) {
-        if (AVMEDIA_TYPE_VIDEO == videoplayer.pAVFormatContext->streams[i]->codec->codec_type) {
-            videoplayer.videoStreamIndex = i;
+    self->stream_index = -1;
+    for (uint i = 0; i < self->format_ctx->nb_streams; i++) {
+        if (AVMEDIA_TYPE_VIDEO == self->format_ctx->streams[i]->codec->codec_type) {
+            self->stream_index = i;
             break;
         }
     }
     
-    if (-1 == videoplayer.videoStreamIndex) {
-        NSLog(@"-1 == videoStreamIndex");
-        avformat_free_context(videoplayer.pAVFormatContext);
-        av_frame_free(&videoplayer.pAVFrame);
+    if (-1 == self->stream_index) {
+        NSLog(@"-1 == stream_index");
+        avformat_free_context(self->format_ctx);
+        av_frame_free(&self->frame);
         return -1;
     }
     
-    AVStream *stream = videoplayer.pAVFormatContext->streams[videoplayer.videoStreamIndex];
-    videoplayer.frame_rate = stream->avg_frame_rate.num/stream->avg_frame_rate.den;
-    NSLog(@"videoplayer.frame_rate %d", videoplayer.frame_rate);
+    AVStream *stream = self->format_ctx->streams[self->stream_index];
+    self->frame_rate = stream->avg_frame_rate.num/stream->avg_frame_rate.den;
+    NSLog(@"frame_rate %d", self->frame_rate);
     
-    videoplayer.pAVCodecContext = videoplayer.pAVFormatContext->streams[videoplayer.videoStreamIndex]->codec;
-    videoplayer.videoWidth = videoplayer.pAVCodecContext->width;
-    videoplayer.videoHeight = videoplayer.pAVCodecContext->height;
+    self->codec_ctx = self->format_ctx->streams[self->stream_index]->codec;
+    self->width = self->codec_ctx->width;
+    self->height = self->codec_ctx->height;
     
-    avpicture_alloc(&videoplayer.pAVPicture, AV_PIX_FMT_RGB24, videoplayer.videoWidth, videoplayer.videoHeight);
+    avpicture_alloc(&self->picture, AV_PIX_FMT_RGB24, self->width, self->height);
     
-    AVCodec *pAVCodec;
+    AVCodec *codec;
     
-    pAVCodec = avcodec_find_decoder(videoplayer.pAVCodecContext->codec_id);
-    videoplayer.pSwsContext = sws_getContext(videoplayer.videoWidth, videoplayer.videoHeight, AV_PIX_FMT_YUV420P, videoplayer.videoWidth, videoplayer.videoHeight, AV_PIX_FMT_RGB24,
-                                 SWS_BICUBIC, 0, 0, 0);
+    codec = avcodec_find_decoder(self->codec_ctx->codec_id);
+    self->sws_ctx = sws_getContext(self->width, self->height, AV_PIX_FMT_YUV420P, self->width, self->height, AV_PIX_FMT_RGB24, SWS_BICUBIC, 0, 0, 0);
     
     NSLog(@"after avcodec_find_decoder");
-    result = avcodec_open2(videoplayer.pAVCodecContext, pAVCodec, NULL);
-    if (result<0){
+    rc = avcodec_open2(self->codec_ctx, codec, NULL);
+    if (rc < 0){
         NSLog(@"avcodec_open2 < 0");
-        avformat_free_context(videoplayer.pAVFormatContext);
-        av_frame_free(&videoplayer.pAVFrame);
-        sws_freeContext(videoplayer.pSwsContext);
-        avpicture_free(&videoplayer.pAVPicture);
+        avformat_free_context(self->format_ctx);
+        av_frame_free(&self->frame);
+        sws_freeContext(self->sws_ctx);
+        avpicture_free(&self->picture);
         return -1;
     }
     return 0;
 }
 
 static int videoplayer_close() {
-    if(videoplayer.isopen) {
-        avformat_free_context(videoplayer.pAVFormatContext);
-        av_frame_free(&videoplayer.pAVFrame);
-        sws_freeContext(videoplayer.pSwsContext);
-        avpicture_free(&videoplayer.pAVPicture);
-        videoplayer.isopen = 0;
+    if(self->is_open) {
+        avformat_free_context(self->format_ctx);
+        av_frame_free(&self->frame);
+        sws_freeContext(self->sws_ctx);
+        avpicture_free(&self->picture);
+        self->is_open = FALSE;
     }
     return 0;
 }
 
 static void videoplayer_rendering() {
-    int frameFinished = 0;
+    int frame_finished = FALSE;
     
     NSDate *date = [NSDate date];
-    NSTimeInterval currentTime = [date timeIntervalSince1970];
+    NSTimeInterval current_time = [date timeIntervalSince1970];
     
-    if (av_read_frame(videoplayer.pAVFormatContext, &videoplayer.pAVPacket) >= 0){
-        if(videoplayer.pAVPacket.stream_index == videoplayer.videoStreamIndex){
-            avcodec_decode_video2(videoplayer.pAVCodecContext, videoplayer.pAVFrame, &frameFinished, &videoplayer.pAVPacket);
-            if (frameFinished){
-                sws_scale(videoplayer.pSwsContext,(const uint8_t* const *)videoplayer.pAVFrame->data,videoplayer.pAVFrame->linesize,0,videoplayer.videoHeight,videoplayer.pAVPicture.data,videoplayer.pAVPicture.linesize);
-                CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault;
-                CFDataRef data = CFDataCreate(kCFAllocatorDefault, videoplayer.pAVPicture.data[0], videoplayer.pAVPicture.linesize[0] * videoplayer.videoHeight);
+    if (av_read_frame(self->format_ctx, &self->packet) >= 0){
+        if(self->packet.stream_index == self->stream_index){
+            avcodec_decode_video2(self->codec_ctx, self->frame, &frame_finished, &self->packet);
+            if (frame_finished){
+                sws_scale(self->sws_ctx, (const uint8_t* const *)self->frame->data, self->frame->linesize, 0, self->height, self->picture.data,self->picture.linesize);
+                CGBitmapInfo bitmap_info = kCGBitmapByteOrderDefault;
+                CFDataRef data = CFDataCreate(kCFAllocatorDefault, self->picture.data[0], self->picture.linesize[0] * self->height);
                 
                 CGDataProviderRef provider = CGDataProviderCreateWithCFData(data);
-                CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-                CGImageRef cgImage = CGImageCreate(videoplayer.videoWidth,
-                                                   videoplayer.videoHeight,
+                CGColorSpaceRef color_space = CGColorSpaceCreateDeviceRGB();
+                CGImageRef cg_image = CGImageCreate(self->width,
+                                                   self->height,
                                                    8,
                                                    24,
-                                                   videoplayer.pAVPicture.linesize[0],
-                                                   colorSpace,
-                                                   bitmapInfo,
+                                                   self->picture.linesize[0],
+                                                   color_space,
+                                                   bitmap_info,
                                                    provider,
                                                    NULL,
                                                    NO,
                                                    kCGRenderingIntentDefault);
                 
-                UIImage *image = [UIImage imageWithCGImage:cgImage];
-                CGImageRelease(cgImage);
-                CGColorSpaceRelease(colorSpace);
+                UIImage *image = [UIImage imageWithCGImage:cg_image];
+                CGImageRelease(cg_image);
+                CGColorSpaceRelease(color_space);
                 CGDataProviderRelease(provider);
                 CFRelease(data);
                 
@@ -166,54 +168,57 @@ static void videoplayer_rendering() {
                 dispatch_async(queue, ^{
                     [ViewController image: image];
                 });
-                ++videoplayer.index;
+                ++self->frame_index;
                 NSDate *date = [NSDate date];
-                NSTimeInterval time0 = [date timeIntervalSince1970];
-                if(videoplayer.time1 == 0 || videoplayer.time1+(0.9/videoplayer.frame_rate) > time0) {
-                    videoplayer.time1 = time0;
-                    NSLog(@"fast frame index: %d", videoplayer.index);
+                NSTimeInterval current_time = [date timeIntervalSince1970];
+                NSTimeInterval interval = 0;
+                
+                if(self->time_frame == 0 || self->time_frame+(0.9/self->frame_rate) > current_time) {
+                    NSLog(@"fast frame index: %d", self->frame_index);
                 } else {
-                    videoplayer.time1 = time0;
-                    [NSThread sleepForTimeInterval:0.9/videoplayer.frame_rate];
-                    NSLog(@"normal frame index: %d", videoplayer.index);
+                    interval = 0.9/self->frame_rate;
+                    NSLog(@"normal frame index: %d", self->frame_index);
                 }
-//                [NSThread sleepForTimeInterval:0.9/videoplayer.frame_rate];
+                self->time_frame = current_time;
+                if(interval > 0) {
+                    [NSThread sleepForTimeInterval:interval];
+                }
             }
         }
-        videoplayer.time0 = currentTime;
+        self->time_restart = current_time;
     } else {
-        if(videoplayer.time0 + 5.0 < currentTime) {
-            videoplayer.restart = 1;
-            videoplayer.time0 = currentTime;
-            ++videoplayer.restart_count;
-            NSLog(@"videoplayer.restart_count %d", videoplayer.restart_count);
+        if(self->time_restart + 5.0 < current_time) {
+            self->restart = TRUE;
+            self->time_restart = current_time;
+            ++self->restart_count;
+            NSLog(@"self->restart_count %d", self->restart_count);
         }
     }
-    av_free_packet(&videoplayer.pAVPacket);
+    av_free_packet(&self->packet);
 }
 
 static void videoplayer_handler() {
     NSDate *date = [NSDate date];
-    NSTimeInterval currentTime = [date timeIntervalSince1970];
-    videoplayer.time0 = currentTime;
-    videoplayer.time1 = 0;
-    videoplayer.restart_count = 0;
-    videoplayer.index = 0;
+    NSTimeInterval current_time = [date timeIntervalSince1970];
+    self->time_restart = current_time;
+    self->time_frame = 0;
+    self->restart_count = 0;
+    self->frame_index = 0;
     
-    while(!videoplayer.stop) {
-        if(!videoplayer.isopen) {
-            NSLog(@"videoplayer is not open %s", videoplayer.uri);
+    while(!self->stop) {
+        if(!self->is_open) {
+            NSLog(@"videoplayer is not open %s", self->uri);
             if(0 == videoplayer_open()) {
-                videoplayer.isopen = 1;
-                NSLog(@"videoplayer_open %s ok", videoplayer.uri);
+                self->is_open = TRUE;
+                NSLog(@"videoplayer_open %s ok", self->uri);
             } else {
-                NSLog(@"videoplayer_open %s fail", videoplayer.uri);
+                NSLog(@"videoplayer_open %s fail", self->uri);
             }
             sleep(1.0);
             continue;
-        } else if(videoplayer.restart) {
+        } else if(self->restart) {
             videoplayer_close();
-            videoplayer.restart = 0;
+            self->restart = TRUE;
             sleep(1.0);
             continue;
         }
@@ -223,22 +228,22 @@ static void videoplayer_handler() {
 }
 
 void videoplayer_init() {
-    videoplayer.queue = dispatch_queue_create("systec.Hello.videoplayer", DISPATCH_QUEUE_SERIAL);
+    self->queue = dispatch_queue_create("systec.Hello.videoplayer", DISPATCH_QUEUE_SERIAL);
 }
 
 void videoplayer_stop() {
-    videoplayer.stop = 1;
+    self->stop = TRUE;
 }
 
 void videoplayer_play(const char *uri) {
     if(strlen(uri) < 4) {
         return;
     }
-    videoplayer.stop = 1;
-    sprintf(videoplayer.uri, "%s", uri);
-    dispatch_async(videoplayer.queue, ^{
-        NSLog(@"videoplayer_play %s", videoplayer.uri);
-        videoplayer.stop = 0;
+    self->stop = TRUE;
+    sprintf(self->uri, "%s", uri);
+    dispatch_async(self->queue, ^{
+        NSLog(@"videoplayer_play %s", self->uri);
+        self->stop = FALSE;
         videoplayer_handler();
     });
 }
